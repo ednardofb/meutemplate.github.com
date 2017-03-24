e9cfa6c56c93f4a3b3441dbab9beddfce8e48596
Ednardo Fernandes <fernandes.ed.ba@gmail.com>
2017-03-24 11:01:57 -0300

    base do projeto

--- javaScript/jas.js
+++ javaScript/jas.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function(){
+	/* banner *******************************************/
+	$('#banner ul').cycle({
+		fx: 'all',
+		speed: 2000,
+		timeout: 5000,
+		next: '#prox',
+		prev:'#ant',
+	});
+	/* Ocultar e apresentar menu ***********************/
+	$('.hidden').hide();
+	$('.submenu').click(function(){
+		$(this).toggleClass("active").next().slideToggle("slow");
+		return false;
+	});
+});
