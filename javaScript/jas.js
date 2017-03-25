1148ffee1e3ad8a280caad7019cfb167d7962d18
Ednardo Fernandes <fernandes.ed.ba@gmail.com>
2017-03-25 15:16:14 -0300

    novas funções

--- javaScript/jas.js
+++ javaScript/jas.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	/* banner *******************************************/
+	/* banner ****************************************************/
 	$('#banner ul').cycle({
 		fx: 'all',
 		speed: 2000,
@@ -7,10 +7,27 @@ $(document).ready(function(){
 		next: '#prox',
 		prev:'#ant',
 	});
-	/* Ocultar e apresentar menu ***********************/
+	/* Ocultar e apresentar menu do Header ***********************/
 	$('.hidden').hide();
 	$('.submenu').click(function(){
 		$(this).toggleClass("active").next().slideToggle("slow");
 		return false;
+	});
+	/* Ocultar e apresentar menu da IMG 1 ***********************/
+	/*Mostrar */
+	$('#mostrar').click(function(event){
+		event.preventDefault();
+		$('#capaefeito').show(1000);
+		$('#capaefeito').css({
+			'background-color': 'rgba(138, 43, 226, 0.8)',
+		});
+	});
+	/*Ocultar */
+	$('#ocultar').click(function(event){
+		event.preventDefault();
+		$('#capaefeito').hide("show");
+		$('#capaefeito').css({
+			'background-color': 'rgba(138, 43, 226, 0.8)',
+		});
 	});
 });
